74803178c2f1ea926992982008e97d056a2ff7dc
hyerin rhee <hyerin rhee>
2016-07-11 19:51:21 +0900

    all remove

--- og_My_Personal_Page/WebContent/script/ex.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function() {
-	$("#change").click(function(){
-		alert("it works");
-//		$("#ex").css('top','300px');
-	});
-});
--- og_My_Personal_Page/WebContent/script/ex_1.js
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-$(document).ready(function() {
-	$("#change").click(function(){
-		alert("it works");
-//		$("#ex").css('top','300px');
-	});
-});
